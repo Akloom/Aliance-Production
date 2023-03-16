80e0246da18dbf67d0476c8109ef2bddd59b4ac4
Code-Monster1 <usupovaliakbar01@gmail.com>
2023-03-16 14:47:08 +0600

    contact done and map done

--- pages/contact.php
+++ pages/contact.php
@@ -53,6 +53,46 @@ if (isset($_POST['send-modal'])) {
 
 <?php include "./layout/name-page.php" ?>
 
+<!-- Блок: контакты -->
+
+<section class="contact container" data-aos="fade-up">
+  <a href="tel:+7 (499) 686-10-14">+7 (499) 686-10-14</a>
+  <div class="contact-content">
+    <div class="contact-content__contacts">
+      <div class="contact-content__contacts-item">
+        <svg class="contact-content__contacts-item__icon">
+          <use href="../assets/img/icons/svg-sprites.svg#nav-icon"></use>
+        </svg>
+        <p>г. Москва, Холодильный пер. 4к1с8</p>
+      </div>
+      <div class="contact-content__contacts-item">
+        <svg class="contact-content__contacts-item__icon">
+          <use href="../assets/img/icons/svg-sprites.svg#mail-icon"></use>
+        </svg>
+        <a href="mailto: a.dragunov@tdaliance.ru">a.dragunov@tdaliance.ru</a>
+      </div>
+    </div>
+    <div class="contact-content__social">
+      <a href="#">
+        <svg class="contact-content__social-icon">
+          <use href="../assets/img/icons/svg-sprites.svg#vk-icon"></use>
+        </svg>
+      </a>
+      <a href="#">
+        <svg class="contact-content__social-icon">
+          <use href="../assets/img/icons/svg-sprites.svg#inst-icon"></use>
+        </svg>
+      </a>
+    </div>
+  </div>
+</section>
+
+<!-- Блок: Карта -->
+
+<section class="map container" data-aos="fade-up">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1461.916148502735!2d74.5938693!3d42.8763837!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7df8c6064d9%3A0x5e0f4e166983dc74!2z0J_RgNC-0YTQtdGB0YHQuNC-0L3QsNC70YzQvdGL0Lkg0LvQuNGG0LXQuSDihJY5OA!5e0!3m2!1sru!2skg!4v1678955953915!5m2!1sru!2skg" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
+
 <!-- Блок: сотрудничество -->
 
 <?php
